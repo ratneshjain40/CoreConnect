b1c4206397ec8ee65e49ca4f8043e322a36e6dbb
Ratnesh <ratneshjain40@gmail.com>
2025-01-04 14:39:15 +0530

    delete with slug

--- src/features/blog/server/service.ts
+++ src/features/blog/server/service.ts
@@ -38,19 +38,23 @@ async function updateBlog(userId: string, data: UpdateBlogType) {
   return blogRepo.updateBlog(blog.id, data);
 }
 
-async function deleteBlog(userId: string, blogId: string) {
-  let blog = await blogRepo.selectBlogById(blogId);
+async function deleteBlog(userId: string, slug: string) {
+  let blog = await blogRepo.getBlogBySlug(slug);
   if (!blog) {
     throw new ErrorResponse('Blog not found');
   }
   if (blog.userId !== userId) {
     throw new ErrorResponse('You are not authorized to delete this blog');
   }
-  return blogRepo.deleteBlog(blogId);
+  return blogRepo.deleteBlog(blog.id);
 }
 
-async function deleteBlogAdmin(blogId: string) {
-  return blogRepo.deleteBlog(blogId);
+async function deleteBlogAdmin(slug: string) {
+    let blog = await blogRepo.getBlogBySlug(slug);
+    if (!blog) {
+      throw new ErrorResponse('Blog not found');
+    }
+  return blogRepo.deleteBlog(blog.id);
 }
 
 export const blogService = {
